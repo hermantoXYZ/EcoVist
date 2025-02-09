50848e6202aec20ec2c05d857e365e3d1b0d0f77
hermantoXYZ <andiupdates@gmail.com>
2025-02-09 16:09:10 +0800

    ps

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,32 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header'
 import SearchInterface from './components/SearchInterface'
 import CategoryGrid from './components/CategoryGrid'
+import AboutUs from './pages/AboutUs'
 import './App.css'
 import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="app-container">
-      <Header />
-      <main className="main-content">
-        <div className="search-section">
-          <h1>Economic Analysis & Computation</h1>
-          <SearchInterface />
-        </div>
-        <CategoryGrid />
-      </main>
-      <Footer />
-    </div>
+    <Router>
+      <div className="app-container">
+        <Header />
+        <Routes>
+          <Route path="/" element={
+            <main className="main-content">
+              <div className="search-section">
+                <h1>Economic Analysis & Computation</h1>
+                <SearchInterface />
+              </div>
+              <CategoryGrid />
+            </main>
+          } />
+          <Route path="/about" element={<AboutUs />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
